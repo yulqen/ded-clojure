10699d38ac33072cef2e816f7216543a0ee14dc6
Matthew Lemon <y@yulqen.org>
2023-01-22 21:04:47 +0000

    trying to fix fixtures

--- test/ded/db_test.clj
+++ test/ded/db_test.clj
@@ -3,17 +3,31 @@
             [clojure.test :refer :all]
             [ded.db :as db]))
 
-(def node (xt/start-node {}))
-
 (def test-site {:xt/id :test-site-1
                 :site/name "Test Site 1"
                 :site/location "Test Location 1"
                 :site/type "Test Type"
                 :site/id 100})
 
+(def test-site2 {:xt/id :test-site2
+                :site/name "Test Site 2"
+                :site/location "Test Location 2"
+                :site/type "Test Type"
+                :site/id 102})
+
 ;; TODO Test some basic middleware functionality
 ;; TODO Add some fixtures here: empty node, node containing sites, etc
 
-(deftest set-up
-  (db/add-site node test-site)
-  (is (+ 1 1)))
+(def ^:dynamic *node*)
+
+(defn with-node [f]
+  (binding [*node* (xt/start-node {})]
+    (xt/submit-tx *node* [[::xt/put test-site2]])
+    (try (f)
+         (finally
+           (.close *node*)))))
+
+(use-fixtures :each with-node)
+
+(deftest test-1
+  (is (= "Nonce" (db/get-site-by-id 100 *node*))))
