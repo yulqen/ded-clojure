9d763c0d807d4a3892e3fd796628c35ca36eab3e
Matthew Lemon <y@yulqen.org>
2023-01-23 16:09:30 +0000

    wip: going to wrong direction in testing

--- test/ded/db_test.clj
+++ test/ded/db_test.clj
@@ -11,6 +11,7 @@
 ;; we use binding in the fixture below to assign this Var to the xtdb node
 ;; and then release it afterwards
 (declare ^:dynamic *node*)
+(declare ^:dynamic *empty-node*)
 
 (defn basic-populated-node
   "Fixture to add data from initial.edn to test database."
@@ -22,7 +23,14 @@
     (f)
     (.close *node*)))
 
-(use-fixtures :each basic-populated-node)
+(defn empty-node
+  "Empty node for testing as an empty database."
+  [f]
+  (binding [*empty-node* (xt/start-node {})]
+    (f)
+    (.close *empty-node*)))
+
+(use-fixtures :each empty-node basic-populated-node)
 
 (deftest get-site-controllers
   (testing "Basic get functions"
@@ -31,5 +39,15 @@
     (is (= #{[:test-site-2 102 "Test Site 2"]
              [:test-site-1 100 "Test Site 1"]}
            (db/get-site-ids *node*)))))
+
+(deftest adding-site-data-to-database
+  (testing "Basic create"
+    (let [first-site (db/add-site *empty-node* {:xt/id :SO1
+                                                :site/name "Tothcubs"})]
+      (is (= 0 (:xtdb.api/tx-id first-site)))
+      (is (= :SO1 (xt/q (xt/db *empty-node*)
+                        '{:find [xtid]
+                          :where [[e :xt/id xtid]]}
+                        ))))))
 
 
