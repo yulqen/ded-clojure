3212f00089128c0631ad2ce832bbff94ff61781b
Matthew Lemon <y@yulqen.org>
2023-01-23 09:15:04 +0000

    able to read initial database data from edn file

--- test/ded/db_test.clj
+++ test/ded/db_test.clj
@@ -1,6 +1,7 @@
 (ns ded.db-test
   (:require [xtdb.api :as xt]
             [clojure.test :refer :all]
+            [clojure.edn :as edn]
             [ded.db :as db]))
 
 (def test-site {:xt/id :test-site-1
@@ -21,15 +22,18 @@
 (declare ^:dynamic *node*)
 
 (defn with-node [f]
-  (binding [*node* (xt/start-node {})]
-    (xt/submit-tx *node* [[::xt/put test-site]])
-    (xt/sync *node*)
-    (f)
-    (.close *node*)))
+  (letfn [(prep-trx [docs]
+            (mapv (fn [doc] [::xt/put doc]) docs))]
+    (binding [*node* (xt/start-node {})]
+      (xt/submit-tx *node* (prep-trx (edn/read-string (slurp "test/ded/initial.edn"))))
+      (xt/sync *node*)
+      (f)
+      (.close *node*))))
 
 (use-fixtures :each with-node)
 
 (deftest test-1
-  (is (= :test-site-1 (first (first (db/get-site-by-id 100 *node*))))))
+  (is (= :test-site-1 (first (first (db/get-site-by-id 100 *node*)))))
+  (is (= :test-site-2 (first (first (db/get-site-by-id 102 *node*))))))
 
 
