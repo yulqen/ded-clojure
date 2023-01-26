1c7e40f59d8e3e5425716f4645370aa9bd754dfd
Matthew Lemon <y@yulqen.org>
2023-01-26 11:09:21 +0000

    brought back EDN-reading code from previous commit into rich-comment

--- test/ded/db_test.clj
+++ test/ded/db_test.clj
@@ -1,6 +1,7 @@
 (ns ded.db-test
   (:require [clojure.test :refer [deftest use-fixtures is testing]]
             [clojure.edn :as edn]
+            [clojure.inspector :as inspector]
             [ded.db :as db]))
 
 ;; TODO Test some basic middleware functionality
@@ -17,4 +18,24 @@
   (is (= (:typecode (db/make-siteop "Test Site 3")) :SOP))
   (is (= (:serial (db/make-siteop "Test Site 1")) 1))
   (is (= (:desc (db/make-siteop "Test Site 1" {:desc "A test"})) "A test")))
+
+;; DESIGN JOURNAL
+#_{:clj-kondo/ignore [:redefined-var]}
+(comment
+  ;; create basic populated node from edn file
+
+  (declare ^:dynamic *node*)
+  (declare ^:dynamic *empty-node*)
+
+  (defn basic-populated-node
+    "Fixture to add data from initial.edn to test database."
+    [f]
+
+    (binding [*node* (xt/start-node {})] ;; start-node from xtd docs
+      (xt/submit-tx *node* (db/prep-trx
+                            (edn/read-string (slurp "test/ded/initial.edn"))))
+      (xt/sync *node*)
+      (f)
+      (.close *node*)))
+  #_())
 
